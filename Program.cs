01f7f9502efe059d754d32416040f92eebff2c35
Angel Nuw <angelnuw7@gmail.com>
2025-09-22 19:00:14 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Assignment12._1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var checker = new RansomChecker();
+
+            Console.WriteLine(checker.CanConstruct("a", "b"));      // false
+            Console.WriteLine(checker.CanConstruct("aa", "ab"));    // false
+            Console.WriteLine(checker.CanConstruct("aa", "aab"));   // true
+        }
+    }
+}
---
+++ RansomChecker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment12._1._1
+{
+    public class RansomChecker
+    {
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            Dictionary<char, int> charcount = new Dictionary<char, int>();
+
+            foreach (char c in magazine)
+            {
+                if (charcount.ContainsKey(c))
+                {
+                    charcount[c]++;
+                }
+                else
+                {
+                    charcount[c] = 1;
+                }
+            }
+
+            foreach (char c in ransomNote)
+            {
+                if (!charcount.ContainsKey(c) || charcount[c] == 0)
+                {
+                    return false;
+                }
+                charcount[c]--;
+            }
+            return true;
+        }
+    }
+}
